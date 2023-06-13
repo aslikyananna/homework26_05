540fad05b6bea0a41045fa2ac031507049ec5f9b
aslikyananna <135046932+aslikyananna@users.noreply.github.com>
2023-06-13 16:15:35 +0400

    Update and rename task3.py to task3 edited.py

---
+++ task3 edited.py
@@ -0,0 +1,10 @@
+var1 = int(input("number1"))
+var2 = int(input("number2"))
+if var1 + var2 != 0:
+    if (var1 - var2) < 0:
+     result = (-1) * (var1 - var2) / var1 + var2
+    else:
+     result = (var1 - var2) / var1 + var2
+    print(result)
+else:
+    print(" denominator of the fraction cannot be 0")
